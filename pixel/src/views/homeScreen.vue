5634dfc4b77575bee6a52432db721ed8997cad31
i-neeed-sleep <kammyl@nycstudents.net>
2026-02-13 14:01:54 -0500

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- pixel/src/views/homeScreen.vue
+++ pixel/src/views/homeScreen.vue
@@ -3,23 +3,30 @@
 </script>
 
 <template>
-    <h1>Run For It</h1>
-    <RouterLink to="/game"><button><img src="./public/startbtn.png"></button></RouterLink>
+    <div>
+        <h1>Run For It</h1>
+        <RouterLink to="/game"><img :class="start" src="@/assets/startbtn.png"></RouterLink>
+        <br>
+    </div>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Jersey+10&display=swap');
 
-button{
-    width: 200px;
-    height: 100px;
-}
 h1{
     font-family: "Jersey 10", sans-serif;
     font-weight: 400;
     font-style: normal;
     text-align: center;
     font-size: 200px;
-    color: #02A9EA;
+    color: #157bdb;
+}
+img{
+    width: 200px;
+    align-self: center;
+}
+div{
+    background-image: url(@/assets/bgArt.png);
+    background-size: 60%;
 }
 </style>
